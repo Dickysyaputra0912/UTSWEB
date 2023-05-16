dcee5fb88719d66ea7d25a3ae0c5f78b534ff7ea
Dicky <you@example.com>
2023-05-17 01:32:54 +0700

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <body>
+
+<div>
+  <img src="../assets/alquran.png " width="300">
+</div>
+  <p>
+    Al-quran menurut terminologi yaitu firman Allah yang mengandung mukjizat yang diturunkan kepada rasul atau Baginda Rasulullah shallallahu alaihi wa sallam nabi Muhammad shallallahu alaihi wasallam dengan perantara malaikat jibril yang tertulis di dalam mushab itu yang disampaikan kepada kita secara mutawatir, yang membacanya dianggap ibadah dimulai dari surat al-fatihah dan diakhiri dengan surat an- nas itu Alquran secara terminologi.
+    Al Qur'an secara bahasa Artinya bacaan yang sempurna ini merupakan pilihan Allah yang sungguh tepat karena tiada satupun ayat sejak manusia mengenal tulisan baca atau baca tulis tulis baca dapat menandingi al-quran yang sangat mulia ada bacaan Alquran yang dapat dibaca ratusan juta orang tidak mengerti atau tidak dapat lulus asalnya dan dihafal oleh orang dewasa remaja dan anak-anak itu menupakan definisi Al Qur'an.
+    Dan suatu kitab atau peringatan yang mempunyai berkah yang telah kita turunkan maka mengapa kamu menginginkannya ini keberkahan buat kita karena Alquran kitab peringatan yang keberkahan yang telah kami turunkan maka mengapa kalian inginkan istilahnya adalah mengandung pengertian mencemoohkan, keberhahan disini adalah suatu kitab yang kami turunkan padamu penuh dengan berkah agar mereka memperhatikan ayat ayatnya dan agar memberikan pelajaran orang yang mempunyai pikiran.
+  </p>
+  </body>
+</template>
+<style>
+ div{
+   text-align: center;
+ }
+</style>
